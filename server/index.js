668d29872c720f7c5e29df0a4c562a7cb22cfd54
“silkyh13” <silkyh13@gmail.com>
2019-10-24 21:05:14 -0400

    felicia does not work properly

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const PORT = 4000;
+const PORT = 666;
 app = express();
 
 app.use(proxy('/api/checkout', {target: 'http://localhost:1234'}));
-app.use(proxy('/products', {target: 'http://localhost:3000'}));
+app.use(proxy('/products', {target: 'http://localhost:3333'}));
+app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
+app.use(proxy('/api/description', {target: 'http://localhost:3000'}));
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
