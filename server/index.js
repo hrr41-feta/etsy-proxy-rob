24bff6f5db59b9610f4e53753acfcef582fa8bce
Rob Nolan <rlnolan15@gmail.com>
2019-11-15 18:42:31 -0500

    changing loaderio get req

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const proxy = require("http-proxy-middleware");
 const PORT = 3500;
 app = express();
 
-app.get("/loaderio-65d26e727c99e49651f8adc06aa751a5", (req, res) => {
-  res.send("loaderio-65d26e727c99e49651f8adc06aa751a5");
-});
-
 app.use(
   proxy("/api/checkout", {
     target: "http://ec2-52-15-159-32.us-east-2.compute.amazonaws.com:1234"
   })
 );
+
+app.get("/loaderio-65d26e727c99e49651f8adc06aa751a5", (req, res) => {
+  res.send("loaderio-65d26e727c99e49651f8adc06aa751a5");
+});
 // app.use(proxy("/products", { target: "http://localhost:3333" }));
 // app.use(proxy("/api/seller", { target: "http://localhost:5000" }));
 // app.use(proxy("/api/description", { target: "http://localhost:3000" }));
